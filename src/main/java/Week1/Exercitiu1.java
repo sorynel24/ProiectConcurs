09c8b54068e6e6efcdb83e1be840e8ab4f887d73
sorynel24 <sorynel255yahoo.com>
2019-12-15 19:32:31 +0200

    First work for this holidays.

--- src/main/java/Week1/Exercitiu1.java
+++
@@ -1,7 +0,0 @@
-package Week1;
-
-public class Exercitiu1 {
-    public static void main(String[] args){
-        System.out.println("Prejbeanu Sorin Marian");
-    }
-}
---
+++ src/main/java/Week1/HomeWork1.java
@@ -0,0 +1,23 @@
+package Week1;
+
+public class HomeWork1 {
+    public static void main(String[] args){
+        System.out.println("Prejbeanu Sorin Marian");
+
+        SecondExe();
+        ThirdExe();
+    }
+
+    public static void SecondExe(){
+        System.out.println("Hello World! \n (And all the people of the world)");
+    }
+
+    public static void ThirdExe(){
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("    *    ");
+    }
+}
