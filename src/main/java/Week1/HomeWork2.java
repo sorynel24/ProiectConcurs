6b5d2fa4e7e5afe6cf02b99c9c7a6e0bc8860e4e
sorynel24 <sorynel255yahoo.com>
2019-12-15 19:59:23 +0200

    First work for this holidays part2.

---
+++ src/main/java/Week1/HomeWork2.java
@@ -0,0 +1,24 @@
+package Week1;
+
+public class HomeWork2 {
+    public static void main(String[] args){
+        String Chicken = "Chicken :";
+        byte NumberOfChicken = 3;
+        String Bacon = "Bacon (kg):";
+        float BaconKg = 5.5f;
+        String TractorWord = "Tractor";
+        String anotherString = "There is none";
+
+        System.out.printf("%s", Chicken);
+        System.out.println();
+        System.out.printf("%d", NumberOfChicken);
+        System.out.println();
+        System.out.printf("%s", Bacon);
+        System.out.println();
+        System.out.println(BaconKg);
+        System.out.printf("%s", TractorWord);
+        System.out.println();
+        System.out.printf("%s", anotherString);
+
+    }
+}
