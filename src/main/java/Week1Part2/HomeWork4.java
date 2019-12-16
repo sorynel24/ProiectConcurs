22ca3db6af62dcf4d101687d4db29e898ca26d4e
sorynel24 <sorynel255yahoo.com>
2019-12-16 20:38:03 +0200

    First work for this holidays part2 in the week1.

---
+++ src/main/java/Week1Part2/HomeWork4.java
@@ -0,0 +1,40 @@
+package Week1Part2;
+
+public class HomeWork4 {
+    public static void main(String[] args){
+
+        //Addition
+        //Part 1
+        int A = 5;
+        int B = 4;
+
+        System.out.printf("%d + %d = %d", A, B, (A + B));
+
+        // part 2
+
+        int C = 73457;
+        int D = 12888;
+
+        System.out.printf("%d + %d = %d", C, D, (C + D));
+
+        //Multiplication
+        //Part 1
+
+        int a = 2;
+        int b = 8;
+
+        System.out.printf("%d + %d = %d", a, b, (a + b));
+
+        //Part 2
+
+        int c = 277;
+        int d = 111;
+
+        System.out.printf("%d + %d = %d", c, d, (c + d));
+
+
+
+
+
+    }
+}
