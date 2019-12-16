6551ba4fc6efda125b77f65beb3fcee0b0ff8580
sorynel24 <44973241+sorynel24@users.noreply.github.com>
2019-12-16 20:44:13 +0200

    Add files via upload

---
+++ HomeWork2.java
@@ -0,0 +1,36 @@
+package Week1;
+
+import java.sql.SQLOutput;
+
+public class HomeWork2 {
+    public static void main(String[] args){
+        String Chicken = "Chicken :";
+        int ChickenNumber = 9000;
+        String Bacon = "Bacon (kg):";
+        float BaconKg = 0.1f;
+        String TractorWord = "Tractor";
+        String anotherString = "Zetor";
+        String anotherString2 = "In a nutshell";
+
+
+        System.out.printf("%s", Chicken);
+        System.out.println();
+        System.out.printf("%d", ChickenNumber);
+        System.out.println();
+        System.out.printf("%s", Bacon);
+        System.out.println();
+        System.out.println(BaconKg);
+        System.out.printf("%s", TractorWord);
+        System.out.println();
+        System.out.printf("%s", anotherString);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.printf("%s", anotherString2);
+        System.out.println();
+        System.out.printf("%d", ChickenNumber);
+        System.out.println();
+        System.out.println(BaconKg);
+        System.out.printf("%s", anotherString);
+    }
+}
