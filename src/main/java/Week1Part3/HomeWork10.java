3d69db627de8734f178ffdd96ac2fe49392c0730
sorynel24 <sorynel255yahoo.com>
2019-12-20 09:38:20 +0200

    A new class added in this package;

---
+++ src/main/java/Week1Part3/HomeWork10.java
@@ -0,0 +1,42 @@
+package Week1Part3;
+
+import nhlstats.NHLStatistics;
+import java.util.Scanner;
+
+public class HomeWork10 {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Top ten by goals");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Top twenty-five players by penalty");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Sydney Crosby statistics");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Philadelphia Flyers statistics");
+        NHLStatistics.teamStatistics("PHI");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Anaheim Ducks statistics by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
+
+
+    }
+}
