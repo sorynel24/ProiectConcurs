cfb032e9669f47becfadff149f485d292107a007
sorynel24 <sorynel255yahoo.com>
2019-12-28 13:49:41 +0200

    The week 2

--- src/main/java/Week2/PrintingLikeABossPart2.java
+++ src/main/java/Week2/PrintingLikeABossPart2.java
@@ -18,10 +18,29 @@ public class PrintingLikeABossPart2 {
     }
 
     public static void xmasTree(int stars){
-        int counteTree = 1;
-        while(counteTree <= stars){
-
-
+        int counterTree = 1;
+        int spacesA = stars - 1;
+        int spacesB = spacesA / 2;
+        while(counterTree <= stars){
+            printSpaces(spacesB);
+            Stars(counterTree);
+            printSpaces(spacesB);
+            System.out.println();
+            counterTree++;
+            counterTree++;
+            spacesB--;
         }
+        int stemFirCounter = 0;
+        stars  = 3;
+        spacesB -= 2;
+        while(stemFirCounter < 2){
+            Stars(stars);
+            printSpaces(spacesB);
+            System.out.println();
+            stemFirCounter++;
+        }
+    }
+    public static void main(String[] args){
+        xmasTree(7);
     }
 }
